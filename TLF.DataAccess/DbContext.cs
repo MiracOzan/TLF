06a638fdb51273639e1b7a1392d27f06aad6ce79
Miraç Ozan <Mira Ozan@MIRA>
2022-05-26 18:00:40 +0300

    Create Data Access Layer

---
+++ TLF.DataAccess/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLF.DataAccess
+{
+    public static class DbContext
+    {
+        public static string ConnectionString = "Data Source = MIRAÇ;" +
+                                                "Initial Catalog = TLFDB;" +
+                                                "Integrated Security=True;";
+    }
+}
